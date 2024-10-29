d74d0f2115ba38946e8a6401813bc4d4c5ad94dd
Vinicius <161778225+Viniciuss1789@users.noreply.github.com>
2024-10-29 20:45:56 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Calculadora from "./assets/components/Calculadora";
+
+function App() {
+  return(
+    <>
+    <Calculadora />
+    </>
+  );
+}
+
+export default App
